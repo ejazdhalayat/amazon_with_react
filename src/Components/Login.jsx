e6240f65c0171e9c76df6b67c506642e43a68cdc
Ejaz-Ul-Huque <ejazdhalayat45@gmail.com>
2023-01-25 23:42:04 +0530

    Button

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -7,10 +7,11 @@ function Login() {
         <div className='flex justify-center  '>
             <img alt='' src={Logo} className='h-28 w-56' />
         </div>
-        <div className='grid  justify-center grid-container '>
+        <div className='grid  justify-center grid-container space-y-3 '>
           <h1 className='text-3xl font-semibold'>Sign in</h1>
-          <p className='mt-2 font-semibold text-sm'>Email or mobile phone number</p>
-          <input className='' placeholder='Enter Your Mail' />
+          <p className=' font-semibold text-sm'>Email or mobile phone number</p>
+          <input className='border border-slate-400 rounded-sm w-72 h-7'  />
+          <button className='  bg-amber-200 h-7 rounded-sm border border-slate-400'>Continue</button>
 
         </div>
 

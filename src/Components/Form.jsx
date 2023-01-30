11c67aea7665ca5069c457029514b5e16af03a69
Ejaz-Ul-Huque <ejazdhalayat45@gmail.com>
2023-01-30 11:27:02 +0530

    form

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -3,10 +3,10 @@ import React from "react";
 function Form() {
   return (
     <div>
-      <section class="text-gray-600 body-font relative bg-[#131921]">
+      <section class="text-gray-600 body-font relative bg-white">
         <div class="container px-5 py-24 mx-auto">
           <div class="flex flex-col text-center w-full mb-12">
-            <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-300">
+            <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-600">
               Product Form
             </h1>
             {/* <p class="lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify.</p> */}
@@ -32,9 +32,9 @@ function Form() {
                     Title
                   </label>
                   <input
-                    type="email"
-                    id="email"
-                    name="email"
+                    // type="email"
+                    // id="email"
+                    // name="email"
                     class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
                   />
                 </div>
@@ -45,9 +45,9 @@ function Form() {
                     Price
                   </label>
                   <input
-                    type="text"
-                    id="email"
-                    name="email"
+                    // type="text"
+                    // id="email"
+                    // name="email"
                     class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
                   />
                 </div>
@@ -58,9 +58,9 @@ function Form() {
                     Seller
                   </label>
                   <input
-                    type="name"
-                    id="email"
-                    name="email"
+                    // type="name"
+                    // id="email"
+                    // name="email"
                     class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
                   />
                 </div>
